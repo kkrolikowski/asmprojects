845a107ca41dc9b432638b80ac549457adb86413
Krzysztof Królikowski <kkrolikowski@gmail.com>
2018-03-17 00:48:15 +0100

    addition operations

---
+++ word/wu.asm
@@ -0,0 +1,39 @@
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+; Simple program demonstrating word-sized arithmetic
+; using unsigned values
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+section .data
+
+; Constants
+EXIT_SUCCESS    equ 0
+sys_EXIT        equ 60
+
+; Variables
+wNum1   dw 7639
+wNum2   dw 23155
+wNum3   dw 16760
+wNum4   dw 7501
+wAns1   dw 0
+wAns2   dw 0
+wAns3   dw 0
+
+section .text
+global _start
+_start:
+; wAns1 = wNum1 + wNum2
+    mov ax, word [wNum1]
+    add ax, word [wNum2]
+    mov word [wAns1], ax
+; wAns2 = wNum1 + wNum3
+    mov ax, word [wNum1]
+    add ax, word [wNum3]
+    mov word [wAns2], ax
+; wAns3 = wNum3 + wNum4
+    mov ax, word [wNum3]
+    add ax, word [wNum4]
+    mov word [wAns3], ax
+last:
+    mov rax, sys_EXIT
+    mov rdi, EXIT_SUCCESS
+    syscall
