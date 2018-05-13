3683fa75c3a678f736f493fb52090ce8d927531f
Krzysztof Królikowski <kkrolikowski@gmail.com>
2018-05-13 10:49:01 +0200

    adding comments

--- listofnumbers/listofnumbers.asm
+++ listofnumbers/listofnumbers.asm
@@ -9,7 +9,7 @@ section .data
 ; Constants
 EXIT_SUCCESS        equ 0
 sys_EXIT            equ 60
-LIMIT               equ 100
+LIMIT               equ 100                         ; Numer of list elements
 
 ; Variables
 list        dd 48272,58356,84260,85036,87892,66686,65157,34419,83447,50058
@@ -23,73 +23,73 @@ list        dd 48272,58356,84260,85036,87892,66686,65157,34419,83447,50058
             dd 56662,68541,57234,40933,83662,36541,76550,56357,82257,97151
             dd 2660,26378,20526,52386,28106,10888,21601,91082,32318,58614
 
-sum_all     dd 0
-sum_divby3  dd 0
-ave_all     dd 0
-min         dd 0
-max         dd 0
-tmp         dd 0
-med         dd 0
+sum_all     dd 0                                    ; sum of all integers
+sum_divby3  dd 0                                    ; sum of integers divisible by 3
+ave_all     dd 0                                    ; average of all integers
+min         dd 0                                    ; minimal list value
+max         dd 0                                    ; maximal list value
+tmp         dd 0                                    ; temporary variable used for calculations
+med         dd 0                                    ; median list value (without sorting)
 
 section .text
 global _start
 _start:
 
 ; Find the median value (without prior sorting)
-    mov rax, LIMIT
-    mov r8, 2
-    mov rdx, 0
-    div r8
-    mov rsi, rax
-    mov rax, 0
-    mov eax, dword [list+rsi*4]
-    mov dword [tmp], eax
-    dec rsi
-    mov eax, dword [list+rsi*4]
-    add eax, dword [tmp]
-    mov edx, 0
-    div r8d
-    mov dword [med], eax
+    mov rax, LIMIT                                  ; obtain the number of all items
+    mov r8, 2                                       ; assume that we have a even numbers
+    mov rdx, 0                                      ; let's find the middle index
+    div r8                                          ; * index = LIMIT / 2
+    mov rsi, rax                                    ; now let's find the first middle value
+    mov rax, 0                                      ;
+    mov eax, dword [list+rsi*4]                     ; * tmp = list[index]
+    mov dword [tmp], eax                            ; the second middle value
+    dec rsi                                         ; * index--;
+    mov eax, dword [list+rsi*4]                     ; * eax = list[index];
+    add eax, dword [tmp]                            ; Now let's sum those two values and divide them by two
+    mov edx, 0                                      ; * eax += list[index]
+    div r8d                                         ; * eax /= 2
+    mov dword [med], eax                            ; To get our middle value: med = eax
 
     mov ecx, LIMIT
-    mov rsi, 0
+    mov rsi, 0                                      ; index = 0;
     mov eax, 0
 ; -----
 ; Calculate the sum of all integers
 
-calcTotalSum:
-    add eax, dword [list+rsi*4]
-    inc rsi
-    loop calcTotalSum
-    mov dword [sum_all], eax
+calcTotalSum:                                       ; while (index < LIMIT)
+    add eax, dword [list+rsi*4]                     ; {
+    inc rsi                                         ;    sum_all += list[index];    
+    loop calcTotalSum                               ;    index++;
+    mov dword [sum_all], eax                        ; }
 ; -----
 ; Calculate the average value of all integes
-    mov edx, 0
-    mov r8d, LIMIT
-    div r8d
+    mov edx, 0                                      ;
+    mov r8d, LIMIT                                  ; ave_all = sum_all / LIMIT
+    div r8d                                         ;
     mov dword [ave_all], eax
 
 ; Reset loop settings
     mov ecx, LIMIT
-    mov rsi, 0
+    mov rsi, 0                                      ; index = 0;
 
 ; Calculate minimum and maximum values
-    mov eax, dword [list]
-    mov dword [min], eax
-    mov dword [max], eax
-FindMinMax:
-    mov eax, dword [list+rsi*4]
-    cmp eax, dword [min]
-    jb NewMin
-    cmp eax, dword [max]
-    ja NewMax
-    inc rsi
-    loop FindMinMax
-    jmp last
-NewMin:
-    mov dword [min], eax
-    inc rsi
-    loop FindMinMax
+    mov eax, dword [list]                           ; 
+    mov dword [min], eax                            ; min = list[0];
+    mov dword [max], eax                            ; max = list[0];
+FindMinMax:                                         ;
+    mov eax, dword [list+rsi*4]                     ; while (index < LIMIT)
+    cmp eax, dword [min]                            ; {
+    jb NewMin                                       ;    if (list[index] < min)
+    cmp eax, dword [max]                            ;    {
+    ja NewMax                                       ;       min = list[index]; 
+    inc rsi                                         ;    }
+    loop FindMinMax                                 ;    if (list[index] > max)
+    jmp last                                        ;    {
+NewMin:                                             ;       max = list[index];
+    mov dword [min], eax                            ;    }
+    inc rsi                                         ;    index++;
+    loop FindMinMax                                 ; }
 NewMax:
     mov dword [max], eax
     inc rsi
