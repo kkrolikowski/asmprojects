eed6b4f56c967f62afef7cc519aad26156113916
Krzysztof Królikowski <kkrolikowski@gmail.com>
2018-10-30 12:28:15 +0100

    gets() function with implementation

--- writefile/wrfile.asm
+++ writefile/wrfile.asm
@@ -12,11 +12,13 @@ NULL                equ 0           ; string termination
 LF                  equ 10          ; newline
 LIMIT               equ 24          ; password length
 
+STDIN               equ 0           ; standard input (keyboard)
 STDOUT              equ 1           ; standard output (screen)
 
 ; ------
 ; Sytem calls
 
+SYS_read            equ 0           ; read() system call code
 SYS_write           equ 1           ; write() system call code
 SYS_exit            equ 60          ; exit() system call code
 
@@ -26,14 +28,26 @@ SYS_exit            equ 60          ; exit() system call code
 header              db "Program saves entered password to a file named: password.txt.", LF
                     db "Password should not exceed 24 characters.", LF, NULL
 prompt              db "Enter password: ", NULL
+EOL                 db LF, NULL     ; end of line
 
 section .bss
+password            resb LIMIT+2    ; storage for provided password
+
 section .text
 global _start
 _start:
     mov rdi, header
     call prints
+    
     mov rdi, prompt
+    call prints
+    mov rdi, password
+    mov esi, LIMIT
+    call gets
+
+    mov rdi, password
+    call prints
+    mov rdi, EOL
     call prints
 
 End:
@@ -76,8 +90,70 @@ printDone:
     pop rbx
     ret
 
+; ----------------------------------------------------------------
+;       int gets(char *str, int len);
+; Args: address of a string (rdi), max password length (rsi)
+; Return: Number of characters in string 
 global gets
 gets:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 17
+    push rbx
+    push r12
+
+; -----
+; Stack map:
+; rbx[0-7]   (64-bit): destination string array address
+; rbx[8-11]  (32-bit): maximum string length
+; rbx[12]    (8-bit):  single character read from standard input
+; rbx[13-16] (32-bit): total characters count provided from standard input
+
+    lea rbx, [rbp-17]
+    mov qword [rbx], rdi            ; save dest. string address
+    mov dword [rbx+8], esi          ; save max string length
+    mov dword [rbx+13], 0           ; characters read
+
+    mov r12, 0                      ; string array index (i=0)
+    inc dword [rbx+8]               ; avoid decreased string limit
+getChrLoop:
+; -----
+; Get single character from standard input
+    mov rax, SYS_read               ; set the read() syscall
+    mov rdi, STDIN                  ; set standard input descriptor
+    lea rsi, byte [rbx+12]          ; set the address of memory to hold character
+    mov rdx, 1                      ; characters count to read
+    syscall                         ; call the kernel
+
+; -----
+; Check if we've reached the end of user input
+    cmp byte [rbx+12], LF           ; if user hits 'enter', line feed character encounters
+    je getChrDone                   ; then we are done with input processing
+
+; -----
+; If we've reached max character limit, then we should
+; clear the input from trailing characters.
+    inc dword [rbx+13]              ; increment total characters count
+    mov r11d, dword [rbx+13]        
+    cmp r11d, dword [rbx+8]
+    jae getChrLoop
+
+; -----
+; Save current character in a destination string array
+    mov al, byte [rbx+12]
+    mov r10, qword [rbx]
+    mov byte [r10+r12], al
+    inc r12                         ; increment array index
+    jmp getChrLoop                  ; go for next character
+
+    mov byte [r10+r12], NULL        ; when all is done, set NULL at the end od string
+    mov rax, r12                    ; return number of characters read.
+
+getChrDone:
+    pop r12
+    pop rbx
+    mov rsp, rbp
+    pop rbp
     ret
 
 global fwrite
